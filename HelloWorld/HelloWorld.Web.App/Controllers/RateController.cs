728008847eeb53e6b73cf944ffb8d449674bdc9e
Fred Sobel <fred.sobel@gmail.com>
2021-06-19 23:15:58 -0500

    add rates example

---
+++ HelloWorld/HelloWorld.Web.App/Controllers/RateController.cs
@@ -0,0 +1,33 @@
+﻿using HelloWorld.Web.App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Web.App.Controllers
+{
+	public class RateController : Controller
+	{
+		private readonly ILogger<RateController> _logger;
+
+
+		public RateController(ILogger<RateController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
